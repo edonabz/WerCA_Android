c6bcc539188f6dc7ac80031435d1164a0e3e5020
edonabz <edo.nabz@gmail.com>
2014-07-23 10:49:40 +0200

    NEW method do fetch notifications:: now a StatusBarNotification[] list is fetched every time a new notification is posted or removed.
    Added logo1.png, ic_launcher.png

--- app/src/main/java/it/codeatlas/werca/WerCANotificationListenerService.java
+++ app/src/main/java/it/codeatlas/werca/WerCANotificationListenerService.java
@@ -10,14 +10,18 @@ import android.util.Log;
 
 public class WerCANotificationListenerService extends NotificationListenerService {
 
-    private StatusBarNotification mStatusbarNotification;
+    //private StatusBarNotification mStatusbarNotification;
     private String currentNotificationPkg;
     private static final String TAG = WerCANotificationListenerService.class.getSimpleName();
 
     private static final String PKG_DIALER = "com.google.android.dialer";
     private static final String PKG_PHONE = "com.android.phone";
     private static final String PKG_MMS = "com.android.mms";
+    private static final String PKG_WHATSAPP = "com.whatsapp";
+    private static final String PKG_TALK = "com.google.android.talk";
     private static final String PKG_GMAIL = "com.google.android.gm";
+
+    private StatusBarNotification[] notificationList;
 
     private int num_calls = 0;
     private int num_msg = 0;
@@ -39,6 +43,54 @@ public class WerCANotificationListenerService extends NotificationListenerServic
         ELP_data = new char[20];
         ELP_data[0] = 'A';
 
+        notificationList = getActiveNotifications();
+
+        num_msg = 0;
+        num_calls = 0;
+        num_email = 0;
+        num_other = 0;
+
+        if (!PKG_DIALER.equals(sbn.getPackageName())) {
+            for (StatusBarNotification notification : notificationList) {       //FOR every notification
+                //Log.d(TAG, "onNotificationRemoved notification.toString" + notification.toString());
+                currentNotificationPkg = notification.getPackageName();
+                Log.d(TAG,"PACKAGE = " + currentNotificationPkg);
+                if (PKG_MMS.equals(currentNotificationPkg) || PKG_WHATSAPP.equals(currentNotificationPkg) || PKG_TALK.equals(currentNotificationPkg)) {
+                    num_msg++;
+                } else if (PKG_PHONE.equals(currentNotificationPkg)) {
+                    num_calls++;
+                } else if (PKG_GMAIL.equals(currentNotificationPkg)) {
+                    num_email++;
+                } else {
+                    num_other++;
+                }
+
+            }
+
+            Log.d(TAG, "onNotificationPosted Call=" + num_calls + " SMS=" + num_msg + " Email=" + num_email + " Altro=" + num_other);
+
+            ELP_data[1] = (char) ('0' + num_msg);
+            ELP_data[2] = (char) ('0' + num_calls);
+            ELP_data[3] = (char) ('0' + num_email);
+            ELP_data[4] = (char) ('0' + num_other);
+            now.setToNow();
+            ELP_data[10] = (char) now.hour;
+            ELP_data[11] = ':';
+            ELP_data[12] = (char) now.minute;
+
+            Intent intent = new Intent();
+            intent.setAction(INTENT_ACTION);
+            intent.putExtra(INTENT_EXTRA, ELP_data);
+            sendBroadcast(intent);
+            Log.d(TAG, "SENT broadcast " + intent.toString());
+            Log.d(TAG, "ELP_data = " + new String(ELP_data));
+
+        }
+
+
+
+        /*
+        //OLD METHOD
         mStatusbarNotification = sbn;
         Log.d(TAG, "Getting Notification");
         currentNotificationPkg = mStatusbarNotification.getPackageName();
@@ -73,11 +125,62 @@ public class WerCANotificationListenerService extends NotificationListenerServic
             Log.d(TAG, "ELP " + new String(ELP_data));
         }
 
+        */
+
+
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
         Time now = new Time();
+        ELP_data = new char[20];
+        ELP_data[0] = 'A';
+
+        notificationList = getActiveNotifications();
+
+        num_msg = 0;
+        num_calls = 0;
+        num_email = 0;
+        num_other = 0;
+
+        if (!PKG_DIALER.equals(sbn.getPackageName())) {
+            for (StatusBarNotification notification : notificationList) {       //FOR every notification
+                //Log.d(TAG, "onNotificationRemoved notification.toString" + notification.toString());
+                currentNotificationPkg = notification.getPackageName();
+                Log.d(TAG,"PACKAGE = " + currentNotificationPkg);
+                if (PKG_MMS.equals(currentNotificationPkg) || PKG_WHATSAPP.equals(currentNotificationPkg) || PKG_TALK.equals(currentNotificationPkg)) {
+                    num_msg++;
+                } else if (PKG_PHONE.equals(currentNotificationPkg)) {
+                    num_calls++;
+                } else if (PKG_GMAIL.equals(currentNotificationPkg)) {
+                    num_email++;
+                } else {
+                    num_other++;
+                }
+
+            }
+
+            Log.d(TAG, "onNotificationRemoved Call=" + num_calls + " SMS=" + num_msg + " Email=" + num_email + " Altro=" + num_other);
+
+            ELP_data[1] = (char) ('0' + num_msg);
+            ELP_data[2] = (char) ('0' + num_calls);
+            ELP_data[3] = (char) ('0' + num_email);
+            ELP_data[4] = (char) ('0' + num_other);
+            now.setToNow();
+            ELP_data[10] = (char) now.hour;
+            ELP_data[11] = ':';
+            ELP_data[12] = (char) now.minute;
+
+            Intent intent = new Intent();
+            intent.setAction(INTENT_ACTION);
+            intent.putExtra(INTENT_EXTRA, ELP_data);
+            sendBroadcast(intent);
+            Log.d(TAG, "SENT broadcast " + intent.toString());
+            Log.d(TAG, "ELP_data = " + new String(ELP_data));
+
+        }
+
+        /*Time now = new Time();
         ELP_data[0] = 'A';
         mStatusbarNotification = sbn;
         Log.e(TAG, "Getting Notification");
@@ -115,7 +218,7 @@ public class WerCANotificationListenerService extends NotificationListenerServic
             Log.d(TAG, "ELP " + new String(ELP_data));
             //sendELP(ELP_data);
         }
-
+*/
     }
 
 }
